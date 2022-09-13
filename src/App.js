3704946acded55104b4204d3c629f2b12a681b0f
J0kk3 <joakim_88@msn.com>
2022-09-13 21:17:47 +0200

    [FINAL] uploaded finshed project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 
-const App = () =>
+import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+
+
+function App ()
 {
+  const isAuth = useSelector( state => state.auth.isAuthenticated );
+
   return (
-    <h1>Hello User!</h1>
+    <Fragment>
+      <Header />
+      { !isAuth && <Auth /> }
+      { isAuth && <UserProfile /> }
+      <Counter />
+    </Fragment>
   );
-};
+}
 
 export default App;
